fb086d6b7a8570c2639e7ca5de46d634f968ff6e
ravimaini <ravindermaini33@gmail.com>
2022-04-05 00:06:54 -0400

    String 70%

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -1,12 +1,30 @@
 package string_problems;
 
 public class Permutation {
-
-    /*
+/*
     Create a Java program to compute all permutations of any given string
 
         e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
     // Implement Here
+    public static void main(String[] args) {
+permutation("ABC");
+    }
+    public static void permutation(String input){
+        permutation("",input);
+    }
+
+    private static void permutation(String permu, String word){
+        if(word.isEmpty()){
+            System.out.println(permu+word);
+        }
+        else{
+            for (int i=0; i<word.length(); i++){
+                permutation(permu+word.charAt(i),word.substring(0,i)+word.substring(i+1,word.length()));
+            }
+        }
+    }
+
+
 }
